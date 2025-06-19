c70ba4c12830ac3f1a1771429c90e28b992f8396
Julián Cardozo <julian@pambumedia.com>
2025-06-19 00:21:36 -0300

    Enhance simulator and add startup test

---
+++ recoleccion/src/main/java/com/micuota/recoleccion/dto/RecoleccionDTO.java
@@ -0,0 +1,10 @@
+package com.micuota.recoleccion.dto;
+
+import java.time.LocalDateTime;
+
+public class RecoleccionDTO {
+    public String contenedorId;
+    public double lat;
+    public double lon;
+    public LocalDateTime recolectado;
+}
