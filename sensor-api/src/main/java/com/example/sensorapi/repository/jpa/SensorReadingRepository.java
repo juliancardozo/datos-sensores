151c88a3602155d6935cc6f66325c04ba778630f
Julián Cardozo <julian@pambumedia.com>
2025-06-20 10:49:16 -0300

    Use UUID string ids for sensor readings

--- sensor-api/src/main/java/com/example/sensorapi/repository/jpa/SensorReadingRepository.java
+++ sensor-api/src/main/java/com/example/sensorapi/repository/jpa/SensorReadingRepository.java
@@ -3,5 +3,5 @@ package com.example.sensorapi.repository.jpa;
 import com.example.sensorapi.model.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
+public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
 }
--- sensor-api/src/main/java/com/example/sensorapi/repository/mongo/SensorReadingMongoRepository.java
+++ sensor-api/src/main/java/com/example/sensorapi/repository/mongo/SensorReadingMongoRepository.java
@@ -3,5 +3,5 @@ package com.example.sensorapi.repository.mongo;
 import com.example.sensorapi.model.SensorReading;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface SensorReadingMongoRepository extends MongoRepository<SensorReading, Long> {
+public interface SensorReadingMongoRepository extends MongoRepository<SensorReading, String> {
 }
