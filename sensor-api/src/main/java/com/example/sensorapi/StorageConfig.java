bbdf04e06093717f68573391e7a7c1eee68beafc
Julián Cardozo <julian@pambumedia.com>
2025-06-20 12:06:36 -0300

    Simplify sensor-api to use Mongo only

--- sensor-api/src/main/java/com/example/sensorapi/StorageConfig.java
+++ sensor-api/src/main/java/com/example/sensorapi/StorageConfig.java
@@ -1,9 +1,7 @@
 package com.example.sensorapi;
 
 import com.example.sensorapi.repository.mongo.SensorReadingMongoRepository;
-import com.example.sensorapi.repository.jpa.SensorReadingRepository;
 import com.example.sensorapi.repository.MongoSensorReadingStore;
-import com.example.sensorapi.repository.SqlSensorReadingStore;
 import com.example.sensorapi.repository.SensorReadingStore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -12,13 +10,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class StorageConfig {
     @Bean
-    @ConditionalOnProperty(name = "sensor.storage.type", havingValue = "sql", matchIfMissing = true)
-    public SensorReadingStore sqlStore(SensorReadingRepository repository) {
-        return new SqlSensorReadingStore(repository);
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "sensor.storage.type", havingValue = "mongo")
+    @ConditionalOnProperty(name = "sensor.storage.type", havingValue = "mongo", matchIfMissing = true)
     public SensorReadingStore mongoStore(SensorReadingMongoRepository repository) {
         return new MongoSensorReadingStore(repository);
     }
--- sensor-api/src/main/java/com/example/sensorapi/repository/jpa/SensorReadingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.sensorapi.repository.jpa;
-
-import com.example.sensorapi.model.SensorReading;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
-}
