9c87eedbbecdb9908d5ba1af43e77236bcc7ded4
Julián Cardozo <julian@pambumedia.com>
2025-06-20 10:42:10 -0300

    Separate JPA and Mongo repos

---
+++ sensor-api/src/main/java/com/example/sensorapi/repository/jpa/SensorReadingRepository.java
@@ -0,0 +1,7 @@
+package com.example.sensorapi.repository.jpa;
+
+import com.example.sensorapi.model.SensorReading;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SensorReadingRepository extends JpaRepository<SensorReading, Long> {
+}
