53b4e28016402b27546ee5e35ad1f6966bf3f1b2
Julián Cardozo <julian@pambumedia.com>
2025-06-20 11:33:38 -0300

    Add Swagger and Postman collection

--- recoleccion/src/main/java/com/micuota/recoleccion/MonitoreoApplication.java
+++ recoleccion/src/main/java/com/micuota/recoleccion/MonitoreoApplication.java
@@ -2,8 +2,11 @@ package com.micuota.recoleccion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Recoleccion API", version = "v1"))
 public class MonitoreoApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitoreoApplication.class, args);
